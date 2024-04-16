700cafe42c36f180da00e694ec620367a9bb76c7
anderson ramos <andersonrms15@gmail.com>
2024-04-16 12:11:12 -0300

    feat: configure eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({ 
-    port: env.PORT, 
-    host: '0.0.0.0' //host app front-end
-}).then(() => {
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0', // host app front-end
+  })
+  .then(() => {
     console.log(`HTTP server runing in port ${env.PORT}`)
-})
+  })
